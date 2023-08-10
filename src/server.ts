0472673931773613be71d38a9e22a59f7a1318eb
Nikita Bulokhov <bulokhov.nikita@gmail.com>
2023-08-10 20:23:27 +0200

    added logging for requests

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,36 @@
 import express from 'express';
 import morgan from 'morgan';
+import fs from 'fs';
+import path from 'path';
 import kidsCamp from './routes/payment-kids-camp';
-import testRoute from './routes/test';
 import emailRouter from './routes/email';
-
 
 const app = express();
 
-app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api', testRoute, kidsCamp, emailRouter);
+const logDir = path.join(__dirname, '..', 'logs');
+
+// Check if directory exists, if not create it
+if (!fs.existsSync(logDir)) {
+  fs.mkdirSync(logDir);
+}
+
+// Define a custom token named 'body'
+morgan.token('body', function (req, res) {
+  return JSON.stringify(req.body);
+});
+
+const logFormat = ':method :url :status :response-time ms - Body:\n :body';
+app.use(
+  morgan(logFormat, {
+    stream: fs.createWriteStream(path.join(logDir, 'request.log'), {
+      flags: 'a',
+    }),
+  })
+);
+
+app.use('/api', kidsCamp, emailRouter);
 
 export default app;
