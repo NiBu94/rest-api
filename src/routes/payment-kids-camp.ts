2a8a9776c0094950218e756711068b952b132aeb
Nikita Bulokhov <bulokhov.nikita@gmail.com>
2023-08-15 21:17:12 +0200

    testing

--- src/routes/payment-kids-camp.ts
+++ src/routes/payment-kids-camp.ts
@@ -8,7 +8,8 @@ paymentKidsCampt.post('/saferpay', async (req, res, next) => {
     // Example Price: 100 CHF => 1.00 CHF
     const price = req.body.fields.totalPrice.value * 100;
     const data = await sendSaferpayRequest(price);
-    res.json(data);
+    // res.json(data);
+    res.redirect(data.RedirectUrl);
   } catch (e) {
     next(e);
   }
