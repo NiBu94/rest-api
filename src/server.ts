92dbdce2d01da0fd25eff737fc60480b6326ffb9
Nikita Bulokhov <bulokhov.nikita@gmail.com>
2023-08-16 18:14:46 +0200

    added cors

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import { winstonLogger, morganLogger } from './configs/loggers';
 import paymentKidsCampt from './routes/payment-kids-camp';
 import emailRouter from './routes/email';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
