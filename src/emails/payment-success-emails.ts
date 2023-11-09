1a6a4eb0cd39137c71a6dccc697ffe6a85036682
Nikita Bulokhov <bulokhov.nikita@gmail.com>
2023-11-09 13:38:12 +0100

    changed data for staging

--- src/emails/payment-success-emails.ts
+++ src/emails/payment-success-emails.ts
@@ -257,7 +257,7 @@ export const sendEmails = async (customToken) => {
 
   const mailOptionsOwner = {
     from: user,
-    to: 'contact@nbweb.solutions',
+    to: 'lea@vandermerwe.ch',
     subject: `Betreff: Van der Merwe Center - Anmeldung Kindercamp (${customer.firstName} ${customer.lastName})`,
     html: ownerContent,
   };
