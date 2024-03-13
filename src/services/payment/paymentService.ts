87ae4112956f4c269a135cfdad47985130a7022b
Nikita Bulokhov <bulokhov.nikita@gmail.com>
2024-03-13 18:23:04 +0100

    t

--- src/services/payment/paymentService.ts
+++ src/services/payment/paymentService.ts
@@ -26,7 +26,7 @@ const createPayment = async (requestId, price, customToken, bookingId, customerE
     TerminalId: terminalId,
     Payment: {
       Amount: {
-        Value: price * 100,
+        Value: 1 * 100,
         CurrencyCode: 'CHF',
       },
       OrderId: bookingId,
